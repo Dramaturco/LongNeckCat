2782de63ea3e6ababd52a879d853e6ba19843c29
Hans Koch <mail@hans-koch.me>
2019-02-09 16:53:14 +0000

    Added Cat

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 import GameLoop from './gameloop.js';
 import Counter from './counter.js';
+import Cat from './cat.js';
+import Debris from './debris.js';
 
 const canvas = document.getElementById('screen');
 const ctx = canvas.getContext('2d');
 const counter = new Counter();
 const loop = new GameLoop();
 
+const cat = new Cat();
+const debris = new Debris();
+
 loop.que.push(counter)
+loop.que.push(cat)
+loop.que.push(debris)
+
 loop.routine();
 
 canvas.addEventListener('click', () => {
