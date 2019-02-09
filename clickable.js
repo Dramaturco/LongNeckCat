127fc5a626d6a9de08c57ca95d15096216424001
Emre Izgoel <izgoel@e-spirit.com>
2019-02-09 18:20:15 +0100

    fixed a few bugs in clickable object

--- clickable.js
+++ clickable.js
@@ -12,18 +12,21 @@ ClickableObject.prototype.setPos = function(x,y){
     this.pos.y = y;
 }
 ClickableObject.prototype.draw = function(context) {
+    console.log("draw")
+    //this gets called but no image appears
     context.drawImage(this.img, this.posX, this.posY);
 }
-ClickableObject.prototype.clicked = function(clicked){
-    if(clicked.x > this.pos.x && clicked.x < this.pos.x + this.img.width){
-        if(clicked.y > this.pos.y && clicked.y < this.pos.y + this.img.height){
+ClickableObject.prototype.clicked = function(clickPos){
+    if(clickPos.x > this.pos.x && clickPos.x < this.pos.x + this.img.width){
+        if(clickPos.y > this.pos.y && clickPos.y < this.pos.y + this.img.height){
             return true;
         }
     }
     return false;
 }
 ClickableObject.prototype.update = function(){
-    if(clicked(window.GameManager.state.lastClick)){
+    if(this.clicked(window.GameManager.state.lastClicked)){
+        //TODO: actually delete this object
         console.log('you hit me');
     }
 }
