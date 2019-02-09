cd5d2dfea58c0c49630e10aeaf65ec13143cdd3d
Hans Koch <mail@hans-koch.me>
2019-02-09 12:18:28 +0000

    init

---
+++ main.js
@@ -0,0 +1,10 @@
+const canvas = document.getElementById('screen');
+const ctx = canvas.getContext('2d');
+
+console.log('hello world');
+
+ctx.beginPath();
+ctx.moveTo(11.5, 0);
+ctx.lineTo(11.5, 11);
+ctx.stroke();
+
