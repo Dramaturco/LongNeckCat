6b93fe979af520d53bc46c9a67103da350feb236
Emre Izgoel <izgoel@e-spirit.com>
2019-02-09 14:10:27 +0100

    added rudimentary counter and font to display

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
+import Counter from './counter.js';
+
 const canvas = document.getElementById('screen');
 const ctx = canvas.getContext('2d');
-
+const counter = new Counter();
+for(let i=0; i<250; i++){
+    counter.increment()
+}
 console.log('hello world');
 
-ctx.beginPath();
-ctx.moveTo(11.5, 0);
-ctx.lineTo(11.5, 11);
-ctx.stroke();
-
+ctx.font = "30px kittyfont";
+ctx.fillText(counter.getCounter(),100,100);
+console.log(counter.getCounter());
